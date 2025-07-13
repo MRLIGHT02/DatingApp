eaa7419e7faaf258ac9dfd8635fc55ad1b3fca02
MRLIGHT02 <saifali42020@gmail.com>
2025-07-13 19:19:36 +0530

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.Serialization.DataContracts;
+using System.Security.Cryptography.X509Certificates;
+using API.Models;
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+
+
+
+}
+
