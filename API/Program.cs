f62c7c0c6027508c6e94ef085e7618b821770182
MRLIGHT02 <saifali42020@gmail.com>
2025-07-14 23:20:21 +0530

    #

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Register the Datacontext and other services
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
 
 
 var app = builder.Build();
