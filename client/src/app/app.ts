14aa87c2bf4824ad76bc552f5153d9872e12dc6a
MRLIGHT02 <saifali42020@gmail.com>
2025-07-17 11:50:44 +0530

    Add user data fetching in App component
    Updated the App class in app.ts to inject HttpClient and modify the ngOnInit lifecycle method. Removed the users variable declaration and implemented logic to fetch user data from an API endpoint, including error handling and completion logging.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -11,11 +11,15 @@ import { inject } from '@angular/core';
 export class App implements OnInit {
   protected readonly title = signal('client');
   http = inject(HttpClient);
-
+  users: any;
   /**
    * Angular lifecycle hook that is called after data-bound properties are initialized.
    */
   ngOnInit(): void {
-    // Initialization logic here
+    this.http.get('http://localhost:5207/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.error('Error fetching users:', err),
+      complete: () => console.log('User data fetch complete')
+    })
   }
 }
